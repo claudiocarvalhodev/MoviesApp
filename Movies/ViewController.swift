df1f3701bdb57eab4edf0595010c46fe47798b6b
Claudio Carvalho <claudiocarvalhodev@gmail.com>
2020-03-23 08:33:47 -0700

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Claudio Carvalho on 23/03/20.
+//  Copyright © 2020 Claudio Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
